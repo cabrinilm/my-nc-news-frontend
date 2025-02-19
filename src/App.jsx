5c9b266eda6412d373b881b2bee53b9bb13795fa
cabrinilm <luca_cabrinimm@hotmail.com>
2025-02-19 15:54:18 +0000

    userAccountProvider create/app embedded

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,22 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { ArticlesCard } from "../Components/ArticlesCard"
 import { ArticleCard } from "../Components/ArticleCard"
 import './App.css'
-
+import { UserAccountProvider } from "../context/UserAccountProvider";
 
 function App() {
 
 
   return (
+    <UserAccountProvider>
      <Router>
       <Routes>
       <Route path="/" element={<ArticlesCard/>} />
       <Route path="/article/:id" element={<ArticleCard />} />
       </Routes>
      </Router>
-       
+     </UserAccountProvider>
 
-  )
+  );
 }
 
 export default App

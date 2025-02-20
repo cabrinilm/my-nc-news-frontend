0c983845c4abfff25c32b2da9f68de35a20fa5a6
cabrinilm <luca_cabrinimm@hotmail.com>
2025-02-20 16:04:51 +0000

    basic query

--- Components/ArticlesCard.jsx
+++ Components/ArticlesCard.jsx
@@ -6,11 +6,14 @@ import Loading from "./Loading";
 export const ArticlesCard = () => {
   const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [sortBy, setSortBy] = useState("created_at");
+  const [order, setOrder] = useState("desc");
 
   useEffect(() => {
     const fetchArticles = async () => {
       try {
-        const articlesData = await getArticles();
+        const articlesData = await getArticles(sortBy, order);
+        console.log(articlesData.articles)
         setArticles(articlesData.articles || []);
       } catch (error) {
         console.log("Error fetching articles", error);
@@ -20,14 +23,36 @@ export const ArticlesCard = () => {
     };
 
     fetchArticles();
-  }, []);
+  }, [sortBy, order]);
 
   if (loading) return <Loading />;
 
   return (
     <>
       <h2>Articles List</h2>
-       <div className="articles-container">
+
+      
+      <div className="sort-controls">
+        <select
+          className="sort-by"
+          value={sortBy}
+          onChange={(e) => setSortBy(e.target.value)}
+        >
+          <option value="created_at">Date</option>
+         
+        </select>
+
+        <select
+          className="order"
+          value={order}
+          onChange={(e) => setOrder(e.target.value)}
+        >
+          <option value="asc">Ascending</option>
+          <option value="desc">Descending</option>
+        </select>
+      </div>
+
+      <div className="articles-container">
         {articles.map((article) => (
           <Link
             to={`/article/${article.article_id}`}
